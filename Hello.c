b41c2cf3a8da741447594b563acde36596a53f59
69011694 <69011694@kmitl.ac.th>
2026-09-23 14:27:50 +0700

    feature/farewell

--- Hello.c
+++ Hello.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
+    checkin(“Developer A,B,C”);
     greeting(“Developer A,B,C !!”);
     farewell(“Developer A,B,C”);
     return 0;
@@ -16,4 +17,9 @@ int farewell(const char *name)
 {
     printf("Goodbye, %s!\n", name);
     return 0;
+}
+int checkin(const char *name)
+{
+printf("%s are checked in\n", name);
+return 0;
 }
